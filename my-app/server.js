dda4e251d016b0a8bce1f47210ba2235f05762b8
Noel Presti <NoelXP@Gmail.com>
2024-05-29 03:38:15 +0000

    Working helm chart my-app

--- my-app/server.js
+++ my-app/server.js
@@ -4,7 +4,7 @@ const app = express();
 var port = process.env.PORT ? Number.parseInt(process.env.PORT) : 3000;
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    res.send("Hello World, And I can change it here?");
 });
 
 
